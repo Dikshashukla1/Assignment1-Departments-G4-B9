541a894ebf20661919db697ced1aaf52a1a853b4
Dikshashukla1 <106049102+Dikshashukla1@users.noreply.github.com>
2022-12-13 00:16:13 +0530

    Add files via upload

---
+++ Department/src/com/gl/bankapp/department/AdminDepartment.java
@@ -0,0 +1,27 @@
+package com.gl.bankapp.department;
+
+public class AdminDepartment extends SuperDepartment {
+
+	public String departmentName1() {
+		return "Admin Department";
+	}
+
+	public String getTodaysWork1() {
+		return "Complete your documents Submission";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String departmentName() {
+		// TODO Auto-generated method stub
+		return "Admin Department";
+	}
+
+	public String getTodaysWork() {
+		// TODO Auto-generated method stub
+		return "Complete your documents Submission";
+	}
+
+}
